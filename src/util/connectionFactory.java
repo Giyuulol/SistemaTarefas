31a9450b72877feb7f0042af99853b67e8c43604
XicoBr <164006890+XicoBr@users.noreply.github.com>
2025-05-26 06:42:56 -0300

    correcao do connectionFactory

--- src/util/connectionFactory.java
+++ src/util/connectionFactory.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class connetionFactory {
+public class connectionFactory {
     private static final String URL = "jdbc:mysql://localhost:3306/sistema_tarefas?useTimezone=true&serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASSWORD = "";
