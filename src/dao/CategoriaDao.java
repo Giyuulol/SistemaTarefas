f051050114b1e36c14a5eb110c48972fd84fb461
Francisco <168738436+Giyuulol@users.noreply.github.com>
2025-05-27 17:31:53 -0300

    :tada: implementações de categoria e categoriaDAO

--- src/dao/CategoriaDao.java
+++ src/dao/CategoriaDao.java
@@ -1,4 +1,85 @@
 package dao;
 
+import java.sql.;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.cCategoria;
+import util.connectionFactory;
+
 public class CategoriaDao {
+
+    public void adicionar(cCategoria cat) throws Exception {
+        String sql = "INSERT INTO categorias (nome) VALUES (?)";
+        try (Connection conn = connectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setString(1, cat.getNome());
+            stmt.executeUpdate();
+
+            try (ResultSet rs = stmt.getGeneratedKeys()) {
+                if (rs.next()) {
+                    cat.setId(rs.getInt(1));
+                }
+            }
+        }
+    }
+
+    public List<cCategoria> listar() throws Exception {
+        String sql = "SELECT FROM categorias";
+        List<cCategoria> lista = new ArrayList<>();
+        try (Connection conn = connectionFactory.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                lista.add(new cCategoria(
+                        rs.getInt("id"),
+                        rs.getString("nome")
+                ));
+            }
+        }
+
+        return lista;
+    }
+
+    public cCategoria buscar(int id) throws Exception {
+        String sql = "SELECT * FROM categorias WHERE id = ?";
+        try (Connection conn = connectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, id);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return new cCategoria(
+                            rs.getInt("id"),
+                            rs.getString("nome")
+                    );
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public void atualizar(cCategoria cat) throws Exception {
+        String sql = "UPDATE categorias SET nome = ? WHERE id = ?";
+        try (Connection conn = connectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, cat.getNome());
+            stmt.setInt(2, cat.getId());
+            stmt.executeUpdate();
+        }
+    }
+
+    public void remover(int id) throws Exception {
+        String sql = "DELETE FROM categorias WHERE id = ?";
+        try (Connection conn = connectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        }
+    }
 }
